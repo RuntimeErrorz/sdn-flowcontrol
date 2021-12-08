f5ac9a7ae9660093f89219515332242b56637186
Jackpot-UESTC <1059171929@qq.com>
2021-12-08 20:33:19 +0800

    get 3 jsons

---
+++ get.py
@@ -0,0 +1,35 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+
+ip="127.0.0.1"
+auth = HTTPBasicAuth("karaf","karaf")
+headers = {'Accept':'application/json'}
+
+def get(type):
+    get_url = 'http://{}:8181/onos/v1/{}'.format(ip, type)
+    print(get_url)
+    resp = requests.get(url = get_url,headers =headers,auth = auth )
+    return resp.status_code,resp.text
+
+if __name__ == '__main__':
+    list=['links', 'devices', 'hosts']
+    for i in list:
+        status_code,resp = get(i)
+        print(status_code)
+        if status_code == 200:
+            contents = json.loads(resp)
+            with open(i + '.json','w') as j:
+                j.write(json.dumps(contents, indent=2))
+file = "D://test//student.txt"
+with open(file, "r") as f:
+    for line in f.readlines():
+        line = line.strip('\n')
+        result = line.split(' ')
+        if data[0] == id:
+            pwd = result[1]
+	        name = result[2]
+	        gender = result[3]
+	        birthday = result[4]
+	        institute = result[5]
+	        major = result[6]
